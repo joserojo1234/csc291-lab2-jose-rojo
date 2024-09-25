e2a1639ba41f96b899a231a1332edc6bbc7be0e7
joserojo1234 <joserojojose123@gmail.com>
2024-09-25 19:50:40 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#define espresso_coffee_gra, 8
+#define cappucino_coffee_gram 8 
+#define mocha_coffee_gram 8
+#define espresso_water 30
+#define cappucino_water 30
+#define mocha_water 39
+#define espresso_milk 0
+#define cappucino_milk 70
+#define mocha_milk 160
+#define espresso_chocolate_syrup 0
+#define cappucino_chocolate_syrup 0
+#define mocha_chocolate_syrup 30
+#define espresso_prcie 3.5
+#define cappucino_price 4.5
+#define mocha_price 5.5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main  ()
