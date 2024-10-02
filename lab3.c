03b0e64dae6e462bcfaf180426becb8d41ae6783
joserojo1234 <joserojojose123@gmail.com>
2024-10-02 18:36:46 +0400

    Create lab3.c

---
+++ lab3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define espresso_coffee_gra, 8
+#define cappucino_coffee_gram 8 
+#define mocha_coffee_gram 8
+#define espresso_water 30
+#define cappucino_water 30
+#define mocha_water 39
+#define espresso_milk 0
+#define cappucino_milk 70
+#define mocha_milk 160
+#define espresso_chocolate_syrup 0
+#define cappucino_chocolate_syrup 0
+#define mocha_chocolate_syrup 30
+#define espresso_prcie 3.5
+#define cappucino_price 4.5
+#define mocha_price 5.5
+#define admin pass 1234
+#define threshold_es_water 60
+#define threshold_mo_water 40
+#define threshold_ca_water 40
+#define threshold_espre_c 10
+#define threshold_moc_c 20
+#define threshold_cap_c 20 
+#define threshold_cap_milk 90
+#define threshold_moc_milk 180
+#define threshold_espre_water
+#define threshold_espre_water
+
+
+int main (void)
+{    
+    int choice;
+    printf ("choose any option ");
+    printf("1. Order a coffee type");
+    printf("2. Admin mode for the coffee maker operator");
+    printf("3. End the application with execution")
+    scanf ("%d",&choice );
+switch (choice){
+    case 1: 
+        printf("1.order a coffee type ");
+        break 
+    case 2:
+        printf("2. Admin mode for the coffee maker operator");
+        break
+    case 3:
+        printf("3 end the application execution"):
+
+}
+
+}
